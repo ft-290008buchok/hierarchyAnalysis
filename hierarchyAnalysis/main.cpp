accab724bf58eb32d29c6ceffde41893eb0baa9a
ft-290008buchok <nickcig1@gmail.com>
2020-11-17 17:44:55 +0500

    Организация ввода

--- hierarchyAnalysis/main.cpp
+++ hierarchyAnalysis/main.cpp
@@ -11,14 +11,25 @@ int main() {
 	int cr_num;
 	int digit;
 	cin >> cr_num;
-
+	float *coefficients = new float[cr_num];
+	for (int i = 0; i < cr_num; i++) {
+		coefficients[i] = 0;
+	}
 	
 	for (int i = 0; i < cr_num; i++) {
 		cout << "   " << i + 1 << "-й";
 	}
+	cout << "   козфициент";
 	for (int i = 0; i < cr_num; i++) {
 		cout << "\n" << i + 1 << "-й";
+		cout << " ";
+		for (int j = 0; j < cr_num; j ++)
+			cout << "/     ";
 	}
+
+	float numerator;
+	float denominator;
+	float relation;
 
 	COORD position;
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -27,8 +38,13 @@ int main() {
 	SetConsoleCursorPosition(hConsole, position);
 	for (int i = 0; i < cr_num; i++) {
 		for (int j = 0; j < cr_num; j++) {
-			cin >> digit;
-			position.X += 6;
+			cin >> numerator;
+			position.X += 2;
+			SetConsoleCursorPosition(hConsole, position);
+			cin >> denominator;
+			relation = numerator / denominator;
+			coefficients[i] += relation;
+			position.X += 4;
 			SetConsoleCursorPosition(hConsole, position);
 		}
 		position.X = 3;
