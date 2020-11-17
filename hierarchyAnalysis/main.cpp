ea9929dd866f0063e788955f7cc3c069b0a25aac
ft-290008buchok <nickcig1@gmail.com>
2020-11-17 19:54:29 +0500

    Комментарии

--- hierarchyAnalysis/main.cpp
+++ hierarchyAnalysis/main.cpp
@@ -1,9 +1,13 @@
+/*Программа для анализа иерархий методом Томаса Саати
+Сравенеие критериев первого уровня*/
+
 #include<iostream>
 #include <Windows.h>
 
 using namespace std;
 
 void norm(float*, int);
+
 
 int main() {
 	setlocale(LC_ALL, "rus");
@@ -12,11 +16,15 @@ int main() {
 	cout << "Введите колличество критериев оценивания\n";
 	int cr_num, i, j;
 	cin >> cr_num;
+
+	//Массив хранящий значения коэффициентов
 	float *coefficients = new float[cr_num];
 	for (i = 0; i < cr_num; i++) {
 		coefficients[i] = 0;
 	}
 	
+	/*Создание таблицы для ввода матрицы
+	дробей попарного сравнения критериев*/
 	for (i = 0; i < cr_num; i++) {
 		cout << "   " << i + 1 << "-й";
 	}
@@ -32,11 +40,19 @@ int main() {
 	float denominator;
 	float relation;
 
+	//Перемещение курсора по консоли
+	// Объявление необходимой структуры
 	COORD position;
+	// Получение дескриптора устройства стандартного вывода
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	// Установка координат X и Y
 	position.X = 3;
 	position.Y = 5;
+	// Перемещение каретки по заданным координатам
 	SetConsoleCursorPosition(hConsole, position);
+
+	/*Ввод дробей попарного сравнения критериев
+	и их суммирование по строкам*/
 	for (i = 0; i < cr_num; i++) {
 		for (j = 0; j < cr_num; j++) {
 			cin >> numerator;
@@ -53,7 +69,10 @@ int main() {
 		SetConsoleCursorPosition(hConsole, position);
 	}
 
+	//Нормирование и округление сумм по строкам
 	norm(coefficients, cr_num);
+
+	//Вывод коэффициентов
 	position.X = 3 + 6 * cr_num;
 	position.Y = 5;
 	SetConsoleCursorPosition(hConsole, position);
@@ -67,16 +86,26 @@ int main() {
 	return 0;
 }
 
+
+
+
+//Функция нормирует и округляет коэффициенты до 0,01
 void norm(float *mass, int size) {
+	//Сумма всех коэффициентов
 	float sum = 0;
+	//Нормированное, но не округлённое значение коэффициента
 	float normed_c;
+	//Хвост от округления
 	float tail;
+
 	for (int i = 0; i < size; i++)
 		sum += mass[i];
+
+	//Нормирование и округление коэффициентов
 	for (int i = 0; i < size; i++) {
 		normed_c = mass[i] / sum;
 		tail = (normed_c * 100 - (int)(normed_c * 100)) / 100;
-		cout << tail<<endl;
+		//Округление результата
 		if (tail * 1000 >= 5) {
 			normed_c += 0.01;
 		}
